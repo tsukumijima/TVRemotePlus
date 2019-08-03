1778389186990fd9f64db6811ebfc23b349fe5f5
tsukumijima <tsukumijima@gmail.com>
2019-08-03 13:25:49 +0900

    release v1.0.0-rc9

--- htdocs/files/resize.js
+++ htdocs/files/resize.js
@@ -2,15 +2,25 @@
 
     // ウインドウサイズ
     $(window).on('load resize',function() {
+      console.log('resize');
       // 1024px以上
       if ($(window).width() > 1024){
         // ウィンドウを読み込んだ時、リサイズされた時に発動
-        // 何故か上手くいかないので6回繰り返す
-        for (var i = 0; i < 8; i++){
+        // 何故か上手くいかないので8回繰り返す
+        var result = 0;
+        while (true){
           var WindowHeight = $(window).height() - $('#top').height();
-          var width = $('#content-wrap').width();
+          var width = $('section').width();
           var height= $('#dplayer').width() * (9 / 16) + 136; // $('#tweet-box').height()
-          $('section').css('max-width',(width * WindowHeight) / height + "px" );
+          if (result == (width * WindowHeight) / height) break; // 同じならループを抜ける
+          if (width < ($(window).width() / 2)){ // widthが変なとき用
+            $('section').css('max-width', '1250px');
+            break;
+          }
+          result = (width * WindowHeight) / height;
+          console.log('width: ' + width);
+          console.log('result: ' + result);
+          $('section').css('max-width', result + 'px');
         }
       }
     });
