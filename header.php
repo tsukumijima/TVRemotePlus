d3a7b80e60e90856fc695566e5b7da50748b2aca
tsukumijima <nambuline.youe233@gmail.com>
2019-08-23 00:32:19 +0900

    release v1.0.0-rc15

--- header.php
+++ header.php
@@ -104,14 +104,14 @@
 
   <nav id="menu-content">
     <div id="menu-link-wrap">
-      <div id="cast-toggle" class="menu-link" aria-label="ChromeCastを使ってテレビで再生できます" data-balloon-pos="up">
+      <div id="cast-toggle" class="menu-link" aria-label="Chromecastを使ってテレビで再生できます" data-balloon-pos="up">
         <svg style="width: 21px;" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="chromecast" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-chromecast fa-w-16">
           <path fill="currentColor" d="M447.83 64H64a42.72 42.72 0 0 0-42.72 42.72v63.92H64v-63.92h383.83v298.56H298.64V448H448a42.72 42.72 0 0 0 42.72-42.72V106.72A42.72 42.72 0 0 0 448 64zM21.28 383.58v63.92h63.91a63.91 63.91 0 0 0-63.91-63.92zm0-85.28V341a106.63 106.63 0 0 1 106.64 106.66v.34h42.72a149.19 149.19 0 0 0-149-149.36h-.33zm0-85.27v42.72c106-.1 192 85.75 192.08 191.75v.5h42.72c-.46-129.46-105.34-234.27-234.8-234.64z" class="">
           </path>
         </svg>
         <span class="menu-link-href">キャストを開始</span>
       </div>
-      <div id="cast-scan" class="menu-link" aria-label="キャストするChromeCastをスキャンします" data-balloon-pos="up">
+      <div id="cast-scan" class="menu-link" aria-label="キャストするChromecastをスキャンします" data-balloon-pos="up">
         <i class="fas fa-sync-alt"></i>
         <span class="menu-link-href">デバイスをスキャン</span>
       </div>
