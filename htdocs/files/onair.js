9b849fa741c40d4054f2eacdbf66ea63f7135114
tsukumijima <tsukumijima@users.noreply.github.com>
2020-12-12 07:01:56 +0900

    新ニコニコ実況への対応準備（コメント受信の実装完了） bf5c0a413b0c2638cc448cdecef889e8caa8db15 で実装したコードを元に改良して DPlayer 側の処理に統合しました コメント送信もこの後実装予定

--- htdocs/files/onair.js
+++ htdocs/files/onair.js
@@ -1,8 +1,9 @@
-  $(function(){
 
-    commentnumber = 0; // コメ番
-    next_comeban = ''; // リクエストごとのコメ番（初回だけ空にする）
-    var autoscroll = true;  // 自動スクロール中かどうか
+commentnumber = 0; // コメ番
+next_comeban = ''; // リクエストごとのコメ番（初回だけ空にする）
+var autoscroll = true;  // 自動スクロール中かどうか
+
+$(function(){
 
     // jQuery で sleep
     function wait(sec) {
@@ -215,179 +216,13 @@
       document.getElementById('comment-scroll').style.opacity = 0;
 
     });
+});
 
 
-    // ---------------------------------------------------------------------
-    // ニコ生新実況に備えて
+// ---------------------------------------------------------------------
+// ニコ生新実況に備えて
 
-    async function newNicoJK() {
-
-        // 視聴セッションを取得        
-        // await で Ajax が完了するまで待つ
-        const watchsession_info = (await $.ajax({
-            url: '/api/jikkyov2/' + stream,
-            dataType: 'json',
-            cache: false,
-        }));
-
-        // 視聴セッションを取得できなかった
-        if (watchsession_info['result'] === 'error') {
-          console.error('Error: ' + watchsession_info['message']);
-          return;
-        }
-
-        // 視聴セッション構築用の WebSocket の URL
-        const watchsession_userid = watchsession_info['session']['user_id'];
-        const watchsession_websocketurl = watchsession_info['session']['websocket_url'];
-
-        // 視聴セッション構築用の WebSocket
-        const watchsession = new WebSocket(watchsession_websocketurl);
-
-        // 視聴セッション構築用 WebSocket を開いたとき
-        watchsession.addEventListener('open', function(event) {
-
-            // 視聴セッションをリクエスト
-            watchsession.send(JSON.stringify({
-                'type': 'startWatching',
-                'data': {
-                    'stream':{
-                        'quality': 'super_high',
-                        'protocol': 'hls',
-                        'latency': 'low',
-                        'chasePlay': false,
-                    },
-                    'room':{
-                        'protocol': 'webSocket',
-                        'commentable':true,
-                    },
-                    'reconnect': false,
-                },
-            }));
-        });
-      
-        // 視聴セッション構築用 WebSocket でメッセージを受信したとき
-        watchsession.addEventListener('message', function(event){
-            
-            const message = JSON.parse(event.data);
-            // console.log(message);
-
-            if (message.type === 'room') {
-
-                const websocket_url = message.data.messageServer.uri;
-                const websocket_threadid = message.data.threadId;
-                const websocket_postkey = message.data.yourPostKey;
-
-                console.log('PostKey:' + websocket_postkey);
-        
-                // コメントサーバーの WebSocket
-                const websocket = new WebSocket(websocket_url, 'msg.nicovideo.jp#json');
-        
-                // WebSocket を開いたとき
-                websocket.addEventListener('open', function(event) {
-
-                    // コメントの送信をリクエスト
-                    websocket.send(JSON.stringify([
-                        { 'ping': {'content': 'rs:0'} },
-                        { 'ping': {'content': 'ps:0'} },
-                        { 'ping': {'content': 'pf:0'} },
-                        { 'ping': {'content': 'rf:0'} },
-                        {
-                            'thread':{
-                                'thread': websocket_threadid,
-                                'threadkey': websocket_postkey,
-                                'version': '20061206',
-                                'user_id': watchsession_userid,
-                                'res_from': 0,
-                                'with_global': 1,
-                                'scores': 1,
-                                'nicoru': 0,
-                            }
-                        },
-                    ]));
-                });
-        
-                // WebSocket でメッセージを受信したとき
-                // コメントを描画する
-                websocket.addEventListener('message', async event => {
-                    
-                    // コメントを取得
-                    const comment = JSON.parse(event.data)['chat'];
-
-                    // コメントがない or 広告用など特殊な場合は弾く
-                    if (comment === undefined ||
-                        comment['content'] === undefined ||
-                        comment['content'].match(/\/[a-z]+ /)) {
-                        return;
-                    }
-
-                    // 色・位置
-                    let color = '#FFFFFF';  // 色のデフォルト
-                    let position = 'right';  // 位置のデフォルト
-                    if (comment['mail'] !== undefined && comment['mail'] !== null) {
-                        const command = comment['mail'].replace('184', '').split(' ');
-                        for (const item of command) {
-                            if (getCommentColor(item) !== null) {
-                                color = getCommentColor(item);
-                            }
-                            if (getCommentPosition(item) !== null) {
-                                position = getCommentPosition(item);
-                            }
-                        }
-                    }
-
-                    // 描画用の配列に変換
-                    const time = moment.unix(comment['date']).format('HH:mm:ss');
-                    const danmaku = {
-                        text: comment['content'],
-                        color: color,
-                        type: position,
-                    }
-
-                    // 指定された秒数分待つ
-                    await new Promise(r => setTimeout(r, settings['comment_delay'] * 1000));
-
-                    // コメント一覧に表示する（ 768px 以上のみ）
-                    if (document.body.clientWidth > 768){
-                        document.querySelector('#comment-draw-box > tbody').insertAdjacentHTML('beforeend',`
-                            <tr class="comment-live">
-                                <td class="time" align="center">` + time + `</td>
-                                <td class="comment">` + danmaku['text'] + `</td>
-                            </tr>`
-                        );
-
-                        // スクロールする（自動スクロールが有効な場合のみ）
-                        // ゆくゆく全部 behavior: 'smooth' に書き換えたい（ JS 使うと重いので）
-                        if (autoscroll) {
-                            const comment_draw_box = document.querySelector('#comment-draw-box');
-                            comment_draw_box.scroll({
-                                top: comment_draw_box.scrollHeight,
-                                left: 0,
-                                behavior: 'smooth',
-                            });
-                        }
-                    }
-
-                    // コメント描画 (再生時のみ)
-                    if (!dp.video.paused){
-                        // console.log(event.data);
-                        dp.danmaku.draw(danmaku);
-                    }
-
-                    // コメント数が 500 を超えたら
-                    if (document.getElementsByClassName('comment-live').length >= 500){
-                        // 古いコメントを削除
-                        document.getElementsByClassName('comment-live')[0].remove();
-                    }
-
-                });
-
-                // 視聴セッション用の WebSocket はもういらないので閉じる
-                watchsession.close();
-            }
-        });
-
-    };
-    newNicoJK();  // しばらく封印
+function newNicoJKAPIBackend() {
 
     /**
      * ニコニコの色指定を 16 進数カラーコードに置換する
@@ -472,5 +307,255 @@
                 return null;
         }
     }
-  
-  });
+
+    /**
+     * 視聴セッションサーバーに接続し、コメントサーバーへの接続情報を取得する
+     * 接続できたらコメントサーバーへの接続情報の入ったオブジェクトを、
+     * 接続できなかったらエラーメッセージを返す
+     * @return {array} [接続成功: true・接続失敗: false, コメントサーバーへの接続情報の入ったオブジェクト or エラーメッセージ]
+     */
+    async function getCommentServerInfo() {
+
+        // 視聴セッション情報を取得
+        // await で Ajax が完了するまで待つ
+        const watchsession_info = await $.ajax({
+            url: '/api/jikkyov2/' + stream,
+            dataType: 'json',
+            cache: false,
+        });
+
+        // 視聴セッション情報を取得できなかった
+        if (watchsession_info.result === 'error') {
+            return [false, watchsession_info.message];  // 接続失敗
+        }
+
+        // 視聴セッション構築用の WebSocket
+        const watchsession = new WebSocket(watchsession_info.session.websocket_url);
+
+        // 視聴セッション構築用 WebSocket を開いたとき
+        watchsession.addEventListener('open', (event) => {
+
+            // 視聴セッションをリクエスト
+            watchsession.send(JSON.stringify({
+                'type': 'startWatching',
+                'data': {
+                    'stream': {
+                        'quality': 'super_high',
+                        'protocol': 'hls',
+                        'latency': 'low',
+                        'chasePlay': false,
+                    },
+                    'room': {
+                        'protocol': 'webSocket',
+                        'commentable': true,
+                    },
+                    'reconnect': false,
+                },
+            }));
+        });
+
+        // コメントサーバーへの接続情報を取得
+        const commentserver_info = await new Promise((resolve, reject) => {
+            
+            // 視聴セッション構築用 WebSocket でメッセージを受信したとき
+            watchsession.addEventListener('message', (event) => {
+                
+                // 受信したメッセージ
+                const message = JSON.parse(event.data);
+    
+                if (message.type === 'room') {
+
+                    // 視聴セッション用の WebSocket はもういらないので閉じる
+                    watchsession.close();
+
+                    // コメントサーバーへの接続情報の入ったオブジェクトを返す
+                    // デバッグ用で実際には使わないものもある
+                    resolve({
+                        // 視聴セッション情報
+                        'title': watchsession_info.session.title,
+                        'live_id': watchsession_info.session.live_id,
+                        'user_id': watchsession_info.session.user_id,
+                        'user_type': watchsession_info.session.user_type,
+                        'is_login': watchsession_info.session.is_login,
+                        'websocket_url': watchsession_info.session.websocket_url,
+                        // コメントサーバーへの接続情報
+                        'thread_id': message.data.threadId,
+                        'postkey': message.data.yourPostKey,
+                        'commentserver_url': message.data.messageServer.uri,
+                    });
+                }
+            });
+        });
+
+        return [true, commentserver_info];
+    }
+    
+    // コメントサーバーの WebSocket
+    let commentserver;
+
+    // コメントサーバーへの接続情報
+    let commentserver_info;
+
+    // コメントサーバーに接続できたか
+    // イベントを使うために DOM を生成する（実際に投入はしない）
+    let commentserver_connected = document.createElement('div');
+
+    // コメントサーバーへの接続情報を取得
+    getCommentServerInfo().then(([commentserver_connectable, commentserver_info_]) => {
+
+        commentserver_info = commentserver_info_;
+    
+        // 接続可能ならコメントサーバーに接続
+        if (commentserver_connectable) {
+        
+            // コメントサーバーの WebSocket
+            commentserver = new WebSocket(commentserver_info.commentserver_url, 'msg.nicovideo.jp#json');
+    
+            // WebSocket を開いたとき
+            commentserver.addEventListener('open', (event) => {
+    
+                // コメントの送信をリクエスト
+                commentserver.send(JSON.stringify([
+                    { 'ping': {'content': 'rs:0'} },
+                    { 'ping': {'content': 'ps:0'} },
+                    { 'ping': {'content': 'pf:0'} },
+                    { 'ping': {'content': 'rf:0'} },
+                    {
+                        'thread':{
+                            'thread': commentserver_info.thread_id,
+                            'threadkey': commentserver_info.postkey,
+                            'version': '20061206',
+                            'user_id': commentserver_info.user_id,
+                            'res_from': 0,
+                            'with_global': 1,
+                            'scores': 1,
+                            'nicoru': 0,
+                        }
+                    },
+                ]));
+
+                // 接続完了のイベントを発火
+                commentserver_connected.dispatchEvent(
+                    new CustomEvent('success', { detail: commentserver_info})  // detail は一応残してるだけ
+                );
+            });
+    
+            // WebSocket でメッセージを受信したとき
+            // コメントを描画する
+            commentserver.addEventListener('message', async (event) => {
+                
+                // コメントを取得
+                const comment = JSON.parse(event.data)['chat'];
+    
+                // コメントがない or 広告用など特殊な場合は弾く
+                if (comment === undefined ||
+                    comment['content'] === undefined ||
+                    comment['content'].match(/\/[a-z]+ /)) {
+                    return;
+                }
+
+                // 自分のコメントも表示しない
+                if (comment['yourpost'] && comment['yourpost'] === 1) {
+                    return;
+                }
+    
+                // 色・位置
+                let color = '#FFFFFF';  // 色のデフォルト
+                let position = 'right';  // 位置のデフォルト
+                if (comment['mail'] !== undefined && comment['mail'] !== null) {
+                    const command = comment['mail'].replace('184', '').split(' ');
+                    for (const item of command) {
+                        if (getCommentColor(item) !== null) {
+                            color = getCommentColor(item);
+                        }
+                        if (getCommentPosition(item) !== null) {
+                            position = getCommentPosition(item);
+                        }
+                    }
+                }
+    
+                // 描画用の配列に変換
+                const time = moment.unix(comment['date']).format('HH:mm:ss');
+                const danmaku = {
+                    text: comment['content'],
+                    color: color,
+                    type: position,
+                }
+    
+                // 指定された秒数分待つ
+                await new Promise(r => setTimeout(r, settings['comment_delay'] * 1000));
+    
+                // コメント一覧に表示する（ 768px 以上のみ）
+                if (document.body.clientWidth > 768){
+                    document.querySelector('#comment-draw-box > tbody').insertAdjacentHTML('beforeend',`
+                        <tr class="comment-live">
+                            <td class="time" align="center">` + time + `</td>
+                            <td class="comment">` + danmaku['text'] + `</td>
+                        </tr>`
+                    );
+    
+                    // スクロールする（自動スクロールが有効な場合のみ）
+                    // ゆくゆく全部 behavior: 'smooth' に書き換えたい（ JS 使うと重いので）
+                    if (autoscroll) {
+                        const comment_draw_box = document.querySelector('#comment-draw-box');
+                        comment_draw_box.scroll({
+                            top: comment_draw_box.scrollHeight,
+                            left: 0,
+                            behavior: 'smooth',
+                        });
+                    }
+                }
+    
+                // コメント描画 (再生時のみ)
+                if (!dp.video.paused){
+                    dp.danmaku.draw(danmaku);
+                }
+    
+                // コメント数が 500 を超えたら
+                if (document.getElementsByClassName('comment-live').length >= 500){
+
+                    // 古いコメントを削除
+                    document.getElementsByClassName('comment-live')[0].remove();
+                }
+            });
+
+        // 接続不能
+        } else {
+
+            // 接続失敗のイベントを発火
+            commentserver_connected.dispatchEvent(
+                new CustomEvent('error', { detail: commentserver_info})  // detail は一応残してるだけ
+            );
+        }
+    });
+
+    return {
+
+        // 受信
+        read: (options) => {
+
+            // コメントサーバーへの接続に成功
+            commentserver_connected.addEventListener('success', (event) => {
+
+                console.log(commentserver_info);
+
+                // 接続成功を DPlayer に通知
+                options.success([{}]);  // 空のコメントを入れておく
+            });
+
+            // コメントサーバーへの接続に失敗
+            commentserver_connected.addEventListener('error', (event) => {
+
+                console.error('Error: ' + commentserver_info);
+
+                // 接続失敗を DPlayer に通知
+                options.error(commentserver_info);  // エラーメッセージを送信
+            });
+        },
+
+        // 送信
+        send: (options) => {
+            options.success([{}]);  // 空のコメントを入れておく
+        }
+    }
+}
