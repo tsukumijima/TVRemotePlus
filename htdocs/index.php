434a5d9890f1ea44c42f19fd60286d8fe69adc66
tsukumijima <tsukumijima@users.noreply.github.com>
2020-12-15 04:26:10 +0900

    index.php の条件分岐を中かっこを使わない構文に変更した 中かっこ使っても書けるけど恐ろしく視認性が悪かったのでこの際変更

--- htdocs/index.php
+++ htdocs/index.php
@@ -24,7 +24,7 @@
 		// トップページにリダイレクト
 		if ($reverse_proxy){
 			header('Location: '.$reverse_proxy_url.'1/');
-		} else {
+    } else {
 			header('Location: '.$site_url.'1/');
 		}
 		exit;
@@ -45,7 +45,7 @@
 	if (!file_exists($base_dir.'htdocs/stream/stream'.$stream.'.m3u8')){
 		if ($silent == 'true'){
 			copy($offline_silent_m3u8, $base_dir.'htdocs/stream/stream'.$stream.'.m3u8');
-		} else {
+    } else {
 			copy($offline_m3u8, $base_dir.'htdocs/stream/stream'.$stream.'.m3u8');
 		}
 	}
@@ -73,24 +73,20 @@
     volume: 1.0,
     autoplay: true,
     screenshot: true,
-<?php	if ($ini[$stream]['state'] === 'ONAir'){ ?>
-    // apiBackend: newNicoJKAPIBackend(),
-<?php	} //括弧終了 ?>
-<?php	if ($ini[$stream]['state'] !== 'File'){ ?>
-    live: true,
-<?php	} //括弧終了 ?>
+    apiBackend: newNicoJKAPIBackend('<?php echo $ini[$stream]['state']; ?>'),
+    live: <?php echo ($ini[$stream]['state'] !== 'File' ? 'true' : 'false'); ?>,
     loop: true,
     lang: 'ja-jp',
     theme: '#007cff',
     // 読み込むm3u8を指定する
     video: {
-<?php	if ($ini[$stream]['state'] == 'File' and $ini[$stream]['fileext'] != 'ts' and $ini[$stream]['encoder'] == 'Progressive'){ ?>
+<?php	if ($ini[$stream]['state'] == 'File' and $ini[$stream]['fileext'] != 'ts' and $ini[$stream]['encoder'] == 'Progressive'): ?>
       url: '/api/stream/<?php echo $stream; ?>?_=<?php echo time(); ?>',
       type: 'normal'
-<?php	} else { ?>
+<?php	else: ?>
       url: '/stream/stream<?php echo $stream; ?>.m3u8',
       type: 'hls'
-<?php	} //括弧終了 ?>
+<?php	endif; ?>
     },
     // 読み込むdanmaku(コメント)
     danmaku: {
@@ -111,14 +107,14 @@
     dp.subtitle.toggle();
   }, false);
 
-<?php	if ($ini[$stream]['state'] == 'File') { ?>
+<?php	if ($ini[$stream]['state'] == 'File'): ?>
   dp.seek(1);
-<?php	} //括弧終了 ?>
+<?php	endif; ?>
 
           </script>
         </div>
 
-<?php	if (isSettingsItem('twitter_show', true, true)){ ?>
+<?php	if (isSettingsItem('twitter_show', true, true)): ?>
         <div id="tweet-account-box">
           <div id="tweet-account">
             <img id="tweet-account-icon" src="/files/account_default.jpg">
@@ -169,11 +165,11 @@
           </form>
           <div class="line"></div>
         </div>
-<?php	} //括弧終了 ?>
+<?php	endif; ?>
         <div id="tweet-close"></div>
       </div>
 
-<?php	if (isSettingsItem('comment_show', true, true)){ ?>
+<?php	if (isSettingsItem('comment_show', true, true)): ?>
       <div id="sidebar">
         <div id="comment-box">
           <div id="comment-box-header">
@@ -192,59 +188,59 @@
           </div>
         </div>
       </div>
-<?php	} //括弧終了 ?>
+<?php endif; ?>
     </div>
 
     <div id="description">
       <div id="epg-box">
         <div id="epg">
-<?php	if ($ini[$stream]['state'] == 'File') { ?>
+<?php	if ($ini[$stream]['state'] == 'File'): ?>
           <div id="epg-title"><?php echo $ini[$stream]['filetitle']; ?></div>
-<?php	} else if ($ini[$stream]['state'] == 'Offline') { ?>
+<?php	elseif ($ini[$stream]['state'] == 'Offline'): ?>
           <div id="epg-title">配信休止中…</div>
-<?php	} else { ?>
+<?php	else: ?>
           <div id="epg-title">取得中…</div>
-<?php	} //括弧終了 ?>
+<?php	endif; ?>
           <div id="reload-box">
             <a id="reload" aria-label="再生が止まった時に押してください" data-balloon-pos="up">
               <span id="clock"><?php echo $clock; ?></span>
               <i class="fas fa-redo-alt"></i>
             </a>
           </div>
-<?php	if ($ini[$stream]['state'] == 'ONAir') { ?>
+<?php	if ($ini[$stream]['state'] == 'ONAir'): ?>
           <div id="epg-next">
             Next >>> <span id="epg-next-title">取得中…</span> <span id="epg-next-starttime"></span><span id="epg-next-to"></span><span id="epg-next-endtime"></span>
           </div>
-<?php	} //括弧終了 ?>
-<?php	if ($ini[$stream]['state'] == 'File') { ?>
+<?php	endif; ?>
+<?php	if ($ini[$stream]['state'] == 'File'): ?>
           <div id="epg-info"><?php echo $ini[$stream]['fileinfo']; ?></div>
-<?php	} else { ?>
+<?php	else: ?>
           <div id="epg-info"></div>
-<?php	} //括弧終了 ?>
+<?php	endif; ?>
         </div>
 
         <div id="epg-subinfo">
-<?php	if ($ini[$stream]['state'] == 'ONAir'){ ?>
+<?php	if ($ini[$stream]['state'] == 'ONAir'): ?>
           <span id="state" style="color: #007cff;" value="ONAir">● ON Air</span>
           <span id="status"></span>
           <div id="epg-chinfo"> 
-<?php		if ($ini[$stream]['channel'] < 55){ ?>
+<?php		if ($ini[$stream]['channel'] < 55): ?>
             <span id="epg-channel">Ch: <?php echo sprintf('%03d', str_replace('_', '', $ini[$stream]['channel'])).' '.$channel; ?></span>
-<?php		} else { //括弧終了 ?>
+<?php		else: ?>
             <span id="epg-channel">Ch: <?php echo sprintf('%03d', $ini[$stream]['channel']).' '.$channel; ?></span>
-<?php		} //括弧終了 ?>
+<?php		endif; ?>
             <span id="epg-time">
               <span id="epg-starttime"></span> <span id="epg-to"></span> <span id="epg-endtime"></span>
             </span>
           </div>
-<?php	} else if ($ini[$stream]['state'] == 'File') { ?>
+<?php	elseif ($ini[$stream]['state'] == 'File'): ?>
           <span id="status"></span>
           <div id="epg-chinfo"> 
             <span id="state" style="color: #4ECDC4;" value="File">● File</span>
             <span id="epg-channel"><?php echo $ini[$stream]['filechannel']; ?></span>
           </div>
           <span id="epg-time"><?php echo $ini[$stream]['filetime']; ?></span>
-<?php	} else { ?>
+<?php	else: ?>
           <span id="state" style="color: gray;" value="Offline">● Offline</span>
           <span id="status"></span>
           <div id="epg-chinfo">
@@ -252,13 +248,13 @@
               <span id="epg-starttime"></span> <span id="epg-to"></span> <span id="epg-endtime"></span>
             </span>
           </div>
-<?php	} //括弧終了 ?>
+<?php	endif; ?>
 
           <div id="watch">
             <span id="watching">1人が視聴中</span>
-<?php	if ($ini[$stream]['state'] == 'ONAir'){ ?>
+<?php	if ($ini[$stream]['state'] == 'ONAir'): ?>
             <span id="ikioi">実況勢い: -</span>
-<?php	} //括弧終了 ?>
+<?php	endif; ?>
           </div>
         </div>
 
@@ -268,21 +264,21 @@
       </div>
 
       <div id="stream-view-box">
-<?php	foreach ($ini as $key => $value){ // 地デジchの数だけ繰り返す ?>
-<?php		if ($value['state'] != 'Offline' || $key == '1'){ ?>
+<?php	foreach ($ini as $key => $value): // 地デジchの数だけ繰り返す ?>
+<?php		if ($value['state'] != 'Offline' || $key == '1'): ?>
         <div class="stream-view stream-view-<?php echo $key; ?>" data-num="<?php echo $key; ?>" data-url="/<?php echo $key; ?>/">
           <div class="stream-box">
             <div class="stream-number-title">Stream</div><div class="stream-number"><?php echo $key; ?></div>
             <div class="stream-stop <?php echo $value['state'] == 'Offline' ? 'disabled' : ''; ?>">
               <i class="stream-stop-icon far fa-stop-circle"></i>
             </div>
-<?php			if ($value['state'] == 'ONAir'){ ?>
+<?php			if ($value['state'] == 'ONAir'): ?>
             <div class="stream-state blue">● ON Air</div>
-<?php			} else if ($value['state'] == 'File') { ?>
+<?php			elseif ($value['state'] == 'File'): ?>
             <div class="stream-state green">● File</div>
-<?php			} else { ?>
+<?php			else: ?>
             <div class="stream-state">● Offline</div>
-<?php			} //括弧終了 ?>
+<?php			endif; ?>
             <div class="stream-info">
               <div class="stream-title"><?php echo $value['state'] == 'Offline' ? '配信休止中…' : '取得中…'; ?></div>
               <div class="stream-channel">
@@ -292,34 +288,34 @@
             </div>
           </div>
         </div>
-<?php		} //括弧終了 ?>
-<?php	} //括弧終了 ?>
+<?php		endif; ?>
+<?php	endforeach; ?>
       </div>
 
       <div id="information">
-<?php	if (empty($BonDriver_dll) and empty($ch)){ // エラーを吐く ?>
+<?php	if (empty($BonDriver_dll) and empty($ch)): // エラーを吐く ?>
         <div class="error">
           BonDriver とチャンネル設定ファイルが見つからないため、ライブ配信を開始できません。<br>
           ファイルが BonDriver フォルダに正しく配置されているか、確認してください。<br>
         </div>
-<?php	} else if (empty($BonDriver_dll)){ ?>
+<?php	elseif (empty($BonDriver_dll)): ?>
         <div class="error">
           BonDriver が見つからないため、ライブ配信を開始できません。<br>
           ファイルが BonDriver フォルダに正しく配置されているか、確認してください。<br>
         </div>
-<?php	} else if (empty($ch)){ ?>
+<?php	elseif (empty($ch)): ?>
         <div class="error">
           チャンネル設定ファイルが見つからないため、ライブ配信を開始できません。<br>
           ファイルが BonDriver フォルダに正しく配置されているか、確認してください。<br>
         </div>
-<?php	} //括弧終了
+<?php	endif;
 
-		if (empty($EDCB_http_url) or !@file_get_contents($EDCB_http_url.'api/EnumEventInfo', false, $ssl_context)){ // EMWUI ?>
+		if (empty($EDCB_http_url) or !@file_get_contents($EDCB_http_url.'api/EnumEventInfo', false, $ssl_context)): // EMWUI ?>
         <div class="error">
           EEDCB Material WebUI のある URL が正しく設定されていないため、番組情報が表示できません。<br>
           設定ページの「EDCB Material WebUI (EMWUI) のある URL」が正しく設定されているかどうか、確認してください。<br>
         </div>
-<?php	} //括弧終了 ?>
+<?php	endif; ?>
 
         <div id="broadcast-tab-box" class="swiper-container">
           <div id="broadcast-tab" class="swiper-wrapper">
@@ -333,18 +329,18 @@
           <div class="swiper-wrapper">
 
             <nav class="broadcast-nav swiper-slide">
-<?php	foreach ($ch_T as $i => $value){ // 地デジchの数だけ繰り返す ?>
+<?php	foreach ($ch_T as $i => $value): // 地デジchの数だけ繰り返す ?>
 <?php		// リモコン番号が被ってるチャンネル
 			// もうちょっとスマートに実装したかったけどうまくいかなかったのでハードコード
 			$subchcount = substr($i, -1);
 			if ($i > 60){
 				$ch_T_channel = 'Ch: '.sprintf('%02d', intval($i) - 60).$subchcount.'-3';
-			} else if ($i > 40){
+      		} elseif ($i > 40){
 				$ch_T_channel = 'Ch: '.sprintf('%02d', intval($i) - 40).$subchcount.'-2';
-			} else if ($i > 20){
+			} elseif ($i > 20){
 				$ch_T_channel = 'Ch: '.sprintf('%02d', intval($i) - 20).$subchcount.'-1';
 			// 通常
-			} else {
+      		} else {
 				$ch_T_channel = 'Ch: '.sprintf('%02d', intval($i)).$subchcount;
 			}
 ?>
@@ -383,11 +379,11 @@
                 </div>
 
               </div>
-<?php	} //括弧終了 ?>
+<?php	endforeach; ?>
             </nav>
 
             <nav class="broadcast-nav swiper-slide">
-<?php	foreach ($ch_S as $i => $value){ // BSchの数だけ繰り返す ?>
+<?php	foreach ($ch_S as $i => $value): // BSchの数だけ繰り返す ?>
 <?php		$ch_S_channel = 'Ch: '.sprintf('%03d', $i); ?>
               <div id="ch<?php echo $i; ?>" class="broadcast-wrap" data-ch="<?php echo $i; ?>"
                     data-channel="<?php echo $ch_S_channel; ?>" data-name="<?php echo $value; ?>">
@@ -424,11 +420,11 @@
                 </div>
 
               </div>
-<?php	} //括弧終了 ?>
+<?php	endforeach; ?>
             </nav>
 
             <nav class="broadcast-nav swiper-slide">
-<?php	foreach ($ch_CS as $i => $value){ // CSchの数だけ繰り返す ?>
+<?php	foreach ($ch_CS as $i => $value): // CSchの数だけ繰り返す ?>
 <?php		$ch_CS_channel = 'Ch: '.sprintf('%03d', $i); ?>
               <div id="ch<?php echo $i; ?>" class="broadcast-wrap" data-ch="<?php echo $i; ?>"
                     data-channel="<?php echo $ch_CS_channel; ?>" data-name="<?php echo $value; ?>">
@@ -465,7 +461,7 @@
                 </div>
 
               </div>
-<?php	} //括弧終了 ?>
+<?php	endforeach; ?>
             </nav>
 
           </div>
@@ -484,48 +480,48 @@
           <span>ストリーム：</span>
           <div class="select-wrap">
             <select name="stream">
-<?php	if ($stream_current_live == 'true'){ ?>
+<?php	if ($stream_current_live == 'true'): ?>
               <option value="1"<?php if ($stream == '1') echo ' selected'; ?>>Stream 1 - <?php echo getFormattedState($ini, 1, true); ?></option>
               <option value="2"<?php if ($stream == '2') echo ' selected'; ?>>Stream 2 - <?php echo getFormattedState($ini, 2, true); ?></option>
               <option value="3"<?php if ($stream == '3') echo ' selected'; ?>>Stream 3 - <?php echo getFormattedState($ini, 3, true); ?></option>
               <option value="4"<?php if ($stream == '4') echo ' selected'; ?>>Stream 4 - <?php echo getFormattedState($ini, 4, true); ?></option>
-<?php		if (isStreamActive($ini, 2) and isStreamActive($ini, 3) and isStreamActive($ini, 4)){ ?>
-<?php			for ($i = 5; isStreamActive($ini, ($i - 1)); $i++){ ?>
+<?php		if (isStreamActive($ini, 2) and isStreamActive($ini, 3) and isStreamActive($ini, 4)): ?>
+<?php			for ($i = 5; isStreamActive($ini, ($i - 1)); $i++): ?>
               <option value="<?php echo $i; ?>"<?php if ($stream == $i) echo ' selected'; ?>>Stream <?php echo $i; ?> - <?php echo getFormattedState($ini, $i, true); ?></option>
-<?php			} //括弧終了 ?>
-<?php		} //括弧終了 ?>
-<?php	} else { ?>
-<?php		if (!isStreamActive($ini, 1)){ ?>
+<?php			endfor; ?>
+<?php		endif; ?>
+<?php	else: ?>
+<?php		if (!isStreamActive($ini, 1)): ?>
                 <option value="1" selected>Stream 1 - <?php echo getFormattedState($ini, 1, true); ?></option>
                 <option value="2">Stream 2 - <?php echo getFormattedState($ini, 2, true); ?></option>
                 <option value="3">Stream 3 - <?php echo getFormattedState($ini, 3, true); ?></option>
                 <option value="4">Stream 4 - <?php echo getFormattedState($ini, 4, true); ?></option>
-<?php		} else if (!isStreamActive($ini, 2)){ ?>
+<?php		elseif (!isStreamActive($ini, 2)): ?>
                 <option value="1">Stream 1 - <?php echo getFormattedState($ini, 1, true); ?></option>
                 <option value="2" selected>Stream 2 - <?php echo getFormattedState($ini, 2, true); ?></option>
                 <option value="3">Stream 3 - <?php echo getFormattedState($ini, 3, true); ?></option>
                 <option value="4">Stream 4 - <?php echo getFormattedState($ini, 4, true); ?></option>
-<?php		} else if (!isStreamActive($ini, 3)){ ?>
+<?php		elseif (!isStreamActive($ini, 3)): ?>
                 <option value="1">Stream 1 - <?php echo getFormattedState($ini, 1, true); ?></option>
                 <option value="2">Stream 2 - <?php echo getFormattedState($ini, 2, true); ?></option>
                 <option value="3" selected>Stream 3 - <?php echo getFormattedState($ini, 3, true); ?></option>
                 <option value="4">Stream 4 - <?php echo getFormattedState($ini, 4, true); ?></option>
-<?php		} else if (!isStreamActive($ini, 4)){ ?>
+<?php		elseif (!isStreamActive($ini, 4)): ?>
                 <option value="1">Stream 1 - <?php echo getFormattedState($ini, 1, true); ?></option>
                 <option value="2">Stream 2 - <?php echo getFormattedState($ini, 2, true); ?></option>
                 <option value="3">Stream 3 - <?php echo getFormattedState($ini, 3, true); ?></option>
                 <option value="4" selected>Stream 4 - <?php echo getFormattedState($ini, 4, true); ?></option>
-<?php		} //括弧終了 ?>
-<?php		if (isStreamActive($ini, 2) and isStreamActive($ini, 3) and isStreamActive($ini, 4)){ ?>
+<?php		endif; ?>
+<?php		if (isStreamActive($ini, 2) and isStreamActive($ini, 3) and isStreamActive($ini, 4)): ?>
                 <option value="1">Stream 1 - <?php echo getFormattedState($ini, 1, true); ?></option>
                 <option value="2">Stream 2 - <?php echo getFormattedState($ini, 2, true); ?></option>
                 <option value="3">Stream 3 - <?php echo getFormattedState($ini, 3, true); ?></option>
                 <option value="4">Stream 4 - <?php echo getFormattedState($ini, 4, true); ?></option>
-<?php			for ($i = 5; isStreamActive($ini, ($i - 1)); $i++){ ?>
+<?php			for ($i = 5; isStreamActive($ini, ($i - 1)); $i++): ?>
                 <option value="<?php echo $i; ?>"<?php if (!isStreamActive($ini, $i)) echo ' selected'; ?>>Stream <?php echo $i; ?> - <?php echo getFormattedState($ini, $i, true); ?></option>
-<?php			} //括弧終了 ?>
-<?php		} //括弧終了 ?>
-<?php	} //括弧終了 ?>
+<?php			endfor; ?>
+<?php		endif; ?>
+<?php	endif; ?>
             </select>
           </div>
         </div>
@@ -564,11 +560,11 @@
           <span>字幕データ：</span>
           <div class="select-wrap">
             <select name="subtitle">
-<?php		if ($subtitle_default == 'true'){ ?>
+<?php		if ($subtitle_default == 'true'): ?>
               <option value="<?php echo $subtitle_default; ?>">デフォルト (字幕オン)</option>
-<?php		} else { ?>
+<?php		else: ?>
               <option value="<?php echo $subtitle_default; ?>">デフォルト (字幕オフ)</option>
-<?php		} //括弧終了 ?>
+<?php		endif; ?>
               <option value="true">字幕オン</option>
               <option value="false">字幕オフ</option>
             </select>
@@ -579,32 +575,32 @@
           <span>使用 BonDriver：</span>
           <div id="broadcast-BonDriver-T" class="select-wrap">
             <select name="BonDriver">
-<?php		if (!empty($BonDriver_default_T)){ ?>
+<?php		if (!empty($BonDriver_default_T)): ?>
               <option value="default">デフォルトの BonDriver</option>
-<?php		} //括弧終了 ?>
-<?php		foreach ($BonDriver_dll_T as $i => $value){ //chの数だけ繰り返す ?>
+<?php		endif; ?>
+<?php		foreach ($BonDriver_dll_T as $i => $value): //chの数だけ繰り返す ?>
               <option value="<?php echo $value; ?>"><?php echo $value; ?></option>
-<?php		} //括弧終了 ?>
+<?php		endforeach; ?>
             </select>
           </div>
           <div id="broadcast-BonDriver-S" class="select-wrap">
             <select name="BonDriver">
-<?php		if (!empty($BonDriver_default_S)){ ?>
+<?php		if (!empty($BonDriver_default_S)): ?>
               <option value="default">デフォルトの BonDriver</option>
-<?php		} //括弧終了 ?>
-<?php		foreach ($BonDriver_dll_S as $i => $value){ //chの数だけ繰り返す ?>
+<?php		endif; ?>
+<?php		foreach ($BonDriver_dll_S as $i => $value): //chの数だけ繰り返す ?>
               <option value="<?php echo $value; ?>"><?php echo $value; ?></option>
-<?php		} //括弧終了 ?>
+<?php		endforeach; ?>
             </select>
           </div>
         </div>
 
         <div id="button-box" class="broadcast-button-box">
-<?php		if (!empty($BonDriver_dll) and !empty($ch)){ ?>
+<?php		if (!empty($BonDriver_dll) and !empty($ch)): ?>
           <button class="bluebutton" type="submit"><i class="fas fa-play"></i>ストリーム開始</button>
-<?php		} else {?>
+<?php		else: ?>
           <button class="bluebutton" type="submit" disabled><i class="fas fa-play"></i>ストリーム開始</button>
-<?php		} //括弧終了 ?>
+<?php		endif; ?>
           <button class="redbutton" type="button"><i class="fas fa-times"></i>キャンセル</button>
         </div>
 
