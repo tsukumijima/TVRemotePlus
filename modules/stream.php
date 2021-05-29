ab118e26264bc3f8ac5973337c664099cc7df391
tsukumijima <tsukumijima@users.noreply.github.com>
2021-05-30 01:28:16 +0900

    エンコードオプションの調整 NVEncC だけ --preset が balanced ではなくdefault らしい？

--- modules/stream.php
+++ modules/stream.php
@@ -762,7 +762,7 @@
 					' -m hls_segment_filename:stream'.$stream.'-'.date('mdHi').'_%05d.ts'.
 					// 映像
 					' --vbr '.$vb.' --max-bitrate '.$vb_max.' --output-res '.$width.'x'.$height.' --sar '.$sar.
-					' --preset balanced --profile main --interlace tff --vpp-deinterlace normal --cabac'.
+					' --preset default --profile main --interlace tff --vpp-deinterlace normal --cabac'.
 					// 音声
 					' --audio-codec 1?aac'.$dual_mono_mode_other.' --audio-stream 1?:stereo --audio-bitrate '.$ab.' --audio-samplerate '.$samplerate.
 					' --audio-filter volume='.$volume.' --audio-ignore-decode-error 30'.
@@ -793,7 +793,7 @@
 					' -m hls_segment_filename:stream'.$stream.'-'.date('mdHi').'_%05d.ts'.
 					// 映像
 					' --vbr '.$vb.' --max-bitrate '.$vb_max.' --output-res '.$width.'x'.$height.' --sar '.$sar.
-					' --preset default --profile main --interlace tff --vpp-afs preset=default'.
+					' --preset balanced --profile main --interlace tff --vpp-afs preset=default'.
 					// 音声
 					' --audio-codec 1?aac'.$dual_mono_mode_other.' --audio-stream 1?:stereo --audio-bitrate '.$ab.' --audio-samplerate '.$samplerate.
 					' --audio-filter volume='.$volume.' --audio-ignore-decode-error 30'.
