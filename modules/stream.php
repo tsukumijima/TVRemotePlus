3a67a84f93955a0e52aa9ac7a748a9aa037a9929
tsukumi <tsukumijima@users.noreply.github.com>
2024-06-07 05:16:20 +0900

    Update: NVEncC の --vpp-deinterlace は品質が微妙なので --vpp-afs を使う 自分用に変えてたけどコミットしてなかった変更①

--- modules/stream.php
+++ modules/stream.php
@@ -447,7 +447,7 @@
 					' -m hls_segment_filename:stream'.$stream.'-'.date('mdHi').'_%05d.ts'.
 					// 映像
 					' --vbr '.$vb.' --max-bitrate '.$vb_max.' --output-res '.$width.'x'.$height.' --sar '.$sar.
-					' --preset default --profile main --interlace tff --vpp-deinterlace normal --cabac'.
+					' --preset default --profile main --interlace tff --vpp-afs preset=default --cabac'.
 					// 音声
 					' --audio-codec 1?aac#dual_mono_mode=main --audio-stream 1?:stereo --audio-bitrate '.$ab.' --audio-samplerate '.$samplerate.
 					' --audio-filter volume='.$volume.' --audio-ignore-decode-error 30'.
@@ -806,7 +806,7 @@
 					' -m hls_segment_filename:stream'.$stream.'-'.date('mdHi').'_%05d.ts'.
 					// 映像
 					' --vbr '.$vb.' --max-bitrate '.$vb_max.' --output-res '.$width.'x'.$height.' --sar '.$sar.
-					' --preset default --profile main --interlace tff --vpp-deinterlace normal --cabac'.
+					' --preset default --profile main --interlace tff --vpp-afs preset=default --cabac'.
 					// 音声
 					' --audio-codec 1?aac'.$dual_mono_mode_other.' --audio-stream 1?:stereo --audio-bitrate '.$ab.' --audio-samplerate '.$samplerate.
 					' --audio-filter volume='.$volume.' --audio-ignore-decode-error 30'.
